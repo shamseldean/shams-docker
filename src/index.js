b4bc1f6e52e73886cffb1bddf8e532a375eb9a49
shamseldean <eldeanshams@gmail.com>
2023-03-30 14:23:57 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const redis =require('redis')
+const { Client } = require('pg')
+// init app
+const PORT = process.env.PORT || 4000;
+const app = express();
+// connect to redis 
+const REDIS_PORT= '6379';
+const REDIS_HOST= 'redis';
+const URL1={url: `redis://${REDIS_HOST}:${REDIS_PORT}`} ;
+const redisClient = redis.createClient(URL1);
+redisClient.on('error',(err) => console.log('redis client error',err));
+redisClient.on('connect',() => console.log('connect to redis...'));
+redisClient.connect();
+
+// connect db
+const DB_USER ='root';
+const DB_PASSWORD ='example';
+const DB_PORT=27017;
+const DB_HOST='mongo';
+const URL=(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_PORT}:${DB_HOST}`) ;
+mongoose
+    .connect(URL)
+    .then(()=> console.log("connect to db"))
+    .catch((err)=> console.log("fail connection to db",err));
+
+
+app.get('/', (req, res) =>{
+    redisClient.set('products','Shams international');
+    res.send("<hl> Hello Shams  i'm so proud of you<hl>")});
+app.get('/data', async (req, res) =>{
+    const products = await redisClient.get('products');
+    res.send(`<hl> Hello Shams  i'm so proud of you<hl> <h2>${products}<h2>`);
+});
+app.listen(PORT, () => console.log(`app 15 up and running on port:,${PORT}`));
+
+// connection postgres
+// PGUSER='root'
+// PGHOST= 'postgres'
+// PGPASSWORD='example'
+// PGDATABASE='mydb'
+// PGPORT= 5432
+
+
+// const connectionString = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}`
+// const client = new Client({
+//     connectionString,
+// })
+// client
+// .connect()
+// .then(()=> console.log("connect to postgress db"))
+// .catch((err)=> console.log("fail connection to postgress db",err));
+
