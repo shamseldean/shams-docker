7e60e3941404e56ceb9b815db5ed16de38f6d576
shamseldean <eldeanshams@gmail.com>
2023-04-04 12:13:52 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const redis =require('redis')
-const { Client } = require('pg')
+//const { Client } = require('pg')
 // init app
 const PORT = process.env.PORT || 4000;
 const app = express();
-// connect to redis 
-const REDIS_PORT= '6379';
-const REDIS_HOST= 'redis';
-const URL1={url: `redis://${REDIS_HOST}:${REDIS_PORT}`} ;
-const redisClient = redis.createClient(URL1);
-redisClient.on('error',(err) => console.log('redis client error',err));
-redisClient.on('connect',() => console.log('connect to redis...'));
-redisClient.connect();
+
 
 // connect db
 const DB_USER ='root';
 const DB_PASSWORD ='example';
 const DB_PORT=27017;
 const DB_HOST='mongo';
-const URL=(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_PORT}:${DB_HOST}`) ;
+const URL=(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`) ;
 mongoose
     .connect(URL)
-    .then(()=> console.log("connect to db"))
+    .then(()=> console.log("connected to db..."))
     .catch((err)=> console.log("fail connection to db",err));
 
+// connect to redis 
+const REDIS_PORT= '6379';
+const REDIS_HOST= 'redis';
+const URL1={url: `redis://${REDIS_HOST}:${REDIS_PORT}`} ;
+const redisClient = redis.createClient(URL1);
+redisClient.on('error',(err) => console.log('redis client error',err));
+redisClient.on('connect',() => console.log('connected to redis...'));
+redisClient.connect();
 
 app.get('/', (req, res) =>{
     redisClient.set('products','Shams international');
